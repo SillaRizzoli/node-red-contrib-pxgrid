23840f3c9e7ade74e85f1137749e3ee67bb31b48
SillaRizzoli <silla.rizzoli@gmail.com>
2020-02-08 00:43:02 +0100

    First usable version

--- pxgrid.js
+++ pxgrid.js
@@ -2,20 +2,82 @@ module.exports = function(RED) {
     "use strict";
     function PxgridServerNode(n) {
         RED.nodes.createNode(this,n);
+
         var pxgrid = require('node-pxgrid');
-        var tlsNode = RED.nodes.getNode(n.tls);
-        var tlsOpts = {};
-        tlsNode.addTLSOptions(tlsOpts);
-        this.pxgridControlOptions = {
-            host: n.pxgridserver,
-            client: n.clientname,
-            clientCert: tlsOpts.cert,
-            clientKey: tlsOpts.key,
-            caBundle: tlsOpts.ca,
-            clientKeyPassword: tlsOpts.passphrase
+        if (n.tls) {
+            var tlsNode = RED.nodes.getNode(n.tls);
+            var tlsOpts = {};
+            var options = {debug:true};
+            tlsNode.addTLSOptions(tlsOpts);
+            if (tlsOpts.cert && tlsOpts.key && tlsOpts.ca && n.pxgridserver && n.clientname) {
+                this.pxgridControlOptions = {
+                    host: n.pxgridserver,
+                    client: n.clientname,
+                    clientCert: tlsOpts.cert,
+                    clientKey: tlsOpts.key,
+                    caBundle: tlsOpts.ca,
+                    clientKeyPassword: tlsOpts.passphrase
+                }
+                this.pxgridControl = new pxgrid.Control(this.pxgridControlOptions);
+                this.pxgridClient = new pxgrid.Client(this.pxgridControl);
+            }
         }
-        this.pxgridControl = new pxgrid.Control(this.pxgridControlOptions);
-        this.pxgridClient = new pxgrid.Client(this.pxgridControl);
+
+        var node = this;
+        node.connected = false;
+        node.connecting = false;
+        this.users = {};
+
+        this.connect = function (goToSubCallback) {
+            if (!node.connected && !node.connecting) {
+                node.connecting = true;
+                this.pxgridClient.connect(options).then(session => {
+                    this.stompSession = session;
+                    node.connected = true;
+                    node.connecting = false;
+                    goToSubCallback();
+                });
+            }
+        }
+
+        this.register = function(sessionNode) {
+            node.users[sessionNode.id] = sessionNode;
+            if (Object.keys(node.users).length === 1) {
+                node.connect(() => {
+                    var allPxNodes = Object.values(node.users);
+                    for (var onePxNode of allPxNodes) {
+                        onePxNode.emit("connected");
+                    }
+                });
+            } else {
+                sessionNode.emit("connected");
+            }
+        };
+
+        this.deregister = function(sessionNode, done) {
+            delete node.users[sessionNode.id];
+            sessionNode.emit("disconnected");
+            done();
+        }
+
+        this.subscribe = function(sessionNode, msgCallback) {
+            if (sessionNode.type == "pxgrid-sessions") {
+                node.pxgridClient.subscribeToSessions(node.stompSession, msgCallback);
+            } else if (sessionNode.type == "pxgrid-radius-failures") {
+                node.pxgridClient.subscribeToRadiusFailures(node.stompSession, msgCallback);
+            }
+        }
+
+        this.on("close",function() {
+            console.log("Cleaning up....");
+            if (node.connected) {
+                this.pxgridClient.disconnect(this.stompSession);
+            }
+            node.connecting = false;
+            node.connected = false;
+            this.pxgridClient = null;
+            this.pxgridControl = null;
+        });
     }
     RED.nodes.registerType("pxgridconf",PxgridServerNode);
 
@@ -23,60 +85,34 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,n);
         var pxClient = RED.nodes.getNode(n.pxgridconf);
         var node = this;
-        var options = {debug:true};
-        pxClient.pxgridClient.connect(options).then(session =>
-            pxClient.pxgridClient.subscribeToSessions(session, function(message) {
-                node.status({fill:"green",shape:"dot",text:"connected"});
-                var newmsg={}
-                try {
-                    newmsg.payload = JSON.parse(message.body);
-                }
-                catch(e) {
-                    newmsg.payload = message.body;
-                }
-                node.send(newmsg);
-                console.log(message.body);
-            })).catch( err => console.log(err))
-/*
-        node.client.on("connect", function() {
+            if (pxClient) {
+                pxClient.register(node);
+            }
+
+        node.on("close", function() {
+            if (pxClient) {
+                pxClient.deregister(node);
+            }
+        });
+        node.on("connected", function() {
             node.status({fill:"green",shape:"dot",text:"connected"});
+            if (pxClient) {
+                  pxClient.subscribe(this, function(message) {
+                      var newmsg={};
+                      try {
+                          newmsg.payload = JSON.parse(message.body);
+                      }
+                      catch(e) {
+                          newmsg.payload = message.body;
+                      }
+                      node.send(newmsg);
+                      console.log(message.body);
+                  })
+              }
+      
         });
-
-        node.client.on("reconnecting", function() {
-            node.status({fill:"red",shape:"ring",text:"reconnecting"});
-            node.warn("reconnecting");
-        });
-
-        node.client.on("error", function(error) {
-            node.status({fill:"grey",shape:"dot",text:"error"});
-            node.warn(error);
-        });
-*/
-        node.status({fill:"grey",shape:"ring",text:"connecting"});
-/*
-        node.client.connect(function(sessionId) {
-            node.log('subscribing to: '+node.topic);
-            node.client.subscribe(node.topic, function(body, headers) {
-                var newmsg={"headers":headers,"topic":node.topic}
-                try {
-                    newmsg.payload = JSON.parse(body);
-                }
-                catch(e) {
-                    newmsg.payload = body;
-                }
-                node.send(newmsg);
-            });
-        }, function(error) {
-            node.status({fill:"grey",shape:"dot",text:"error"});
-            node.warn(error);
-        });
-*/
-        node.on("close", function(done) {
- /*           if (node.client) {
-                // disconnect can accept a callback - but it is not always called.
-                node.client.disconnect();
-            } */
-            done();
+        node.on("disconnected", function() {
+            node.status({fill:"grey",shape:"dot",text:"disconnected"});
         });
     }
     RED.nodes.registerType("pxgrid-sessions",PxgridSessionFeedNode);
@@ -84,26 +120,37 @@ module.exports = function(RED) {
 
     function PxgridRadiusFailuresNode(n) {
         RED.nodes.createNode(this,n);
+        var pxClient = RED.nodes.getNode(n.pxgridconf);
         var node = this;
-        pxgridClient.connect().then(session =>
-            pxgridClient.subscribeToRadiusFailures(session, function(message) {
-                node.status({fill:"green",shape:"dot",text:"connected"});
-                var newmsg={"headers":headers,"topic":node.topic}
-                try {
-                    newmsg.payload = JSON.parse(message.body);
-                }
-                catch(e) {
-                    newmsg.payload = message.body;
-                }
-                node.send(newmsg);
-                console.log(message.body);
-            })).catch( err => console.log(err))
+            if (pxClient) {
+                pxClient.register(node);
+            }
 
-        node.status({fill:"grey",shape:"ring",text:"connecting"});
-
-        node.on("close", function(done) {
-             done();
+        node.on("close", function() {
+            if (pxClient) {
+                pxClient.deregister(node);
+            }
+        });
+        node.on("connected", function() {
+            node.status({fill:"green",shape:"dot",text:"connected"});
+            if (pxClient) {
+                  pxClient.subscribe(this, function(message) {
+                      var newmsg={};
+                      try {
+                          newmsg.payload = JSON.parse(message.body);
+                      }
+                      catch(e) {
+                          newmsg.payload = message.body;
+                      }
+                      node.send(newmsg);
+                      console.log(message.body);
+                  })
+              }
+      
+        });
+        node.on("disconnected", function() {
+            node.status({fill:"grey",shape:"dot",text:"disconnected"});
         });
     }
-    RED.nodes.registerType("pxgrid-radius-failures",PxgridSessionFeedNode);
+    RED.nodes.registerType("pxgrid-radius-failures",PxgridRadiusFailuresNode);
 }
